cbf0f269cd13ceedfcdb3ab2a61d254789e840ea
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2022-12-19 20:18:18 +0100

    add offensive words function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
 
+function numberOfOccurrencesInText(word, text) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
 
-
-
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
 
 
 
